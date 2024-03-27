3850ccb466f0234303c7f9058f9a71a5d95962da
Prodipta Bose <nielbose1996@gmail.com>
2024-03-27 23:45:57 +0530

    Charts  module added

---
+++ components/Charts.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { PieChart, Pie, Sector, Cell, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const COLORS = ['#FFBB28', '#FF8042', '#0088FE'];
+
+function ExpenseSummary({ data }) {
+  return (
+    <PieChart width={400} height={400}>
+      <Pie
+        data={data}
+        cx={200}
+        cy={200}
+        labelLine={false}
+        label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+        outerRadius={80}
+        fill="#8884d8"
+        dataKey="value"
+      >
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+      <Tooltip />
+    </PieChart>
+  );
+}
+
+
+function ExpenseTrends({ data }) {
+    return (
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 20, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="amount" fill="#8884d8" />
+      </BarChart>
+    );
+  }
+  
+  export { ExpenseSummary, ExpenseTrends };
+  
